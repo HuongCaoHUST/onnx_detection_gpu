a8d5cc447c07e8d2ba3cef588f0295c21afbbfd5
Huong Cao <huongcao.seee@gmail.com>
2026-05-17 08:36:47 +0700

    small fix

--- run_with_display.sh
+++ run_with_display.sh
@@ -52,8 +52,8 @@ else
     echo "Running indefinitely using Webcam (close window to stop)..."
     echo "Usage: $0 [draw_results=true|false] [duration_in_seconds]"
     echo ""
-    /usr/bin/gst-launch-1.0 \
-        filesrc location=./test_video_2.mp4 ! decodebin ! videoconvert ! videorate ! video/x-raw,framerate=25/1 ! tee name=t \
-        t. ! queue max-size-buffers=5 ! onnxoverlay name=ov motion-compensation=linear ! videoconvert ! fpsdisplaysink sync=true \
-        t. ! queue max-size-buffers=3 leaky=downstream ! videorate drop-only=true ! video/x-raw,framerate=25/2 ! videoscale ! video/x-raw,format=RGB,width=640,height=640 ! onnxinference model-location=/home/huongcao/onnx_detection_gpu/yolo11n.onnx ! onnxpostprocess conf-threshold=0.3 nms-threshold=0.45 draw-results=false ! onnxtracker tracker-algorithm=sort ! ov.sink_meta
+    timeout 60 /usr/bin/gst-launch-1.0 -v \
+        filesrc location=./test_video_3.mp4 ! decodebin ! videoconvert ! videorate ! video/x-raw,framerate=25/1 ! tee name=t \
+        t. ! queue max-size-buffers=10 ! onnxoverlay name=ov motion-compensation=false ! videoconvert ! video/x-raw,format=I420 ! x264enc tune=zerolatency ! matroskamux ! filesink location=output_new_6.mkv sync=true \
+        t. ! queue max-size-buffers=2 leaky=downstream ! videorate drop-only=true ! video/x-raw,framerate=25/2 ! videoscale ! video/x-raw,format=RGB,width=640,height=640 ! onnxinference model-location=/home/huongcao/onnx_detection_gpu/yolo11n.onnx ! onnxpostprocess conf-threshold=0.3 nms-threshold=0.45 draw-results=false ! onnxtracker tracker-algorithm=sort ! ov.sink_meta
 fi
